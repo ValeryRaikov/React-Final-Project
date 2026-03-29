aa9b0cab48c09fb03fda339a31ee50a66feb760e
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2026-03-29 16:42:32 +0300

    Added css enhancements

--- client/src/components/item/Item.jsx
+++ client/src/components/item/Item.jsx
@@ -11,15 +11,18 @@ export default function Item({
     available,
     officeIds,
 }) {
+    const shopCount = officeIds?.length || 0;
+    const shopText = shopCount === 1 ? 'shop' : 'shops';
+
     return (
-        <div className="item">
+        <div className={`item ${!available ? 'out-of-stock-item' : ''}`}>
             <div className="item-image-wrapper">
                 <Link to={`/product/${id}`}><img src={image} alt="" /></Link>
                 <div className={`item-availability ${available ? 'in-stock' : 'out-of-stock'}`}>
                     {available ? (
                         <>
                             <span className="availability-icon">✓</span>
-                            <span className="availability-text">{officeIds?.length || 0} shops</span>
+                            <span className="availability-text">{shopCount} {shopText}</span>
                         </>
                     ) : (
                         <>

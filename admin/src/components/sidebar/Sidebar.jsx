8dff6ac43011b2f14cf3428370e06b94e722d06a
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2026-04-01 14:38:58 +0300

    Clean admin sidebar styles

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -1,28 +1,31 @@
 import { Link } from 'react-router-dom';
 
 import './Sidebar.css';
-import add_product_icon from '../assets/Product_Cart.svg';
-import list_product_icon from '../assets/Product_list_icon.svg';
-import edit_pencil_icon from '../assets/pencil_edit.svg';
-import delete_trash_icon from '../assets/delete_trash.svg';
+import add_product_icon from '../assets/cart.svg';
+import list_product_icon from '../assets/checklist.svg';
+import add_admin_icon from '../assets/administrator.svg';
+import list_admin_icon from '../assets/contact-book.svg';
 import add_promocode_icon from '../assets/add_promocode_icon.svg';
-import list_promocodes_icon from '../assets/discount-coupon.svg';
+import list_promocode_icon from '../assets/discount-coupon.svg';
 
 const sidebarLinks = [
     // product links
     { path: '/add-product', label: 'Add Product', icon: add_product_icon },
     { path: '/list-products', label: 'List Products', icon: list_product_icon },
-    { path: '/list-products', label: 'Edit Product', icon: edit_pencil_icon },
-    { path: '/list-products', label: 'Delete Product', icon: delete_trash_icon },
+
+    // admin links
+    { path: '/add-admin', label: 'Add Admin', icon: add_admin_icon },
+    { path: '/list-admins', label: 'List Admins', icon: list_admin_icon },
 
     // promocode links
     { path: '/add-promocode', label: 'Add Promocode', icon: add_promocode_icon },
-    { path: '/list-promocodes', label: 'List Promocodes', icon: list_promocodes_icon },
+    { path: '/list-promocodes', label: 'List Promocodes', icon: list_promocode_icon },
 ];
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
+            <h3 className="sidebar-title">Admin Dashboard</h3>
             {sidebarLinks.map((link, idx) => (
                 <Link to={link.path} key={idx} style={{textDecoration: "none"}}>
                     <div className="sidebar-item">
